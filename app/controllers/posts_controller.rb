09cf12cfc76b6f1a5c3a65ec9a0c32a9ba96129e
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-22 12:29:10 +0300

    Add new page for posts, add methods for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+    before_action :authenticate_member!, except: %i[index]
+
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_member.posts.build(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit([:title, :body])
+    end
+
 end
